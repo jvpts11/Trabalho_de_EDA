79eb9b4c581d55498675aef402b67301e495678a
jvpts11 <jvptsyt11@gmail.com>
2022-03-14 14:40:13 +0000

    Adição dos primeiros Arquivos do trabalho
    it is what it is bro

---
+++ Trabalho_EDA/Trabalho_EDA/IStructs.h
@@ -0,0 +1,11 @@
+/**
+* Autor: João Tavares
+* E-mail: a21871@alunos.ipca.pt
+* Data:14/03/2022
+*/
+
+
+//Struct para definir os jobs
+typedef struct Jobs {
+
+}Jobs;
---
+++ Trabalho_EDA/Trabalho_EDA/T_Main.c
@@ -0,0 +1,23 @@
+/**
+* Autor: João Tavares
+* E-mail: a21871@alunos.ipca.pt
+* Data:14/03/2022
+* 
+* Colocar outros contribuintes aqui abaixo:
+* 
+* 
+*/
+
+//Bibliotecas nativas de C
+#include <stdio.h>
+#include <stdlib.h>
+
+//Interfaces
+#include "IStructs.h"
+
+#define t_main main
+
+//Método main, alterado para dar mais legibilidade
+void t_main() {
+	printf("Hello World");
+}
