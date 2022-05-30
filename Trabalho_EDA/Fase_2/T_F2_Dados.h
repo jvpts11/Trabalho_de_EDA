e5b0b4857f5e1da27cd2165142eade508977cae6
jvpts11 <jvptsyt11@gmail.com>
2022-05-30 09:48:32 +0100

    Mais um pouco da fase 2

---
+++ Trabalho_EDA/Fase_2/T_F2_Dados.h
@@ -0,0 +1,25 @@
+/**
+* @file T_F2_Dados.h
+* @author Joao_Tavares
+* @brief Header File que contém todas as Assinaturas de funções e estruturas
+* @date May 2022
+*/
+
+#pragma once
+
+
+#ifndef DATA
+
+
+#pragma region Estruturas
+
+struct ProcessPlan {
+
+};
+
+typedef struct ProcessPlan p;
+
+#pragma endregion
+
+
+#endif
---
+++ Trabalho_EDA/Fase_2/T_F2_Manipulacao.c
@@ -0,0 +1,13 @@
+/**
+* @file T_F2_Manipulacao.c
+* @author Joao_Tavares
+* @brief .C que contém funções para manipulação dos jobs e operações
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+
+#include "T_F2_Dados.h"
+
