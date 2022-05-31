ce19d3ee6243a4a5b0831a23bdda3188c61d79b1
NeilZKuN <joaogilmonteiro@gmail.com>
2022-05-31 20:01:49 +0100

    aa

--- Trabalho_EDA/Trabalho_EDA/T_Main.c
+++ Trabalho_EDA/Trabalho_EDA/T_Main.c
@@ -19,13 +19,19 @@
 */
 int main()
 {
-	j* job1 = t_j_Cria_Job(1);
-	j* job2 = t_j_Cria_Job(2);
-	j* job3 = t_j_Cria_Job(3);
+	j* job1;
+	j* job2;
+	j* job3;
 	j** headj = job1;
+	job1 = t_j_CriaeInsere_Job(headj, 1);
+	job2 = t_j_CriaeInsere_Job(headj, 2);
+	job3 = t_j_CriaeInsere_Job(headj, 3);
 
+
+	/*
 	job2 = t_j_Inserir_Novo_Job(&headj, job2);
 	job3 = t_j_Inserir_Novo_Job(&headj, job3);
+	
 
 	o* op1_1 = t_o_cria_operacao(1);
 	o* op1_2 = t_o_cria_operacao(2);
@@ -56,20 +62,21 @@ int main()
 	job1->operation = op1_1;
 	job2->operation = op2_1;
 	job3->operation = op3_1;
-
+	*/
 	
 	j* aux;
 	aux = headj;
+	/*
 	o* auxx;
-	auxx = t_o_procurar_Operacao(headop1, 1);
+	auxx = job1->operation;
+	*/
+	
 	while (aux != NULL)
 	{
-		if (aux->numero == 1)
-		{
-			printf("%d\n", auxx->number);
-			auxx = auxx->nextt;
-		}
+		printf("%d\n", aux->numero);
+		aux = aux->next;
 	}
+	
 	return 0;
 	
 }
